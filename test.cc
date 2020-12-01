a4932ae2837a7eba4e3f495bf7201e47de3aa58a
Jamie <jdeng31@ucsc.edu>
2020-11-30 16:51:05 -0800

    update

---
+++ test.cc
@@ -0,0 +1,40 @@
+#include "yen.h"
+using namespace YEN;
+
+int main() 
+{ 
+    // create the graph 
+    int V = 9; 
+    Graph g(V); 
+    // the picture of g is in the link of line 3
+    // edge between u, v. The third int is weight
+    g.addEdge(0, 1, 4); 
+    g.addEdge(0, 7, 8); 
+    g.addEdge(1, 2, 8); 
+    g.addEdge(1, 7, 11); 
+    g.addEdge(2, 3, 7); 
+    g.addEdge(2, 8, 2); 
+    g.addEdge(2, 5, 4); 
+    g.addEdge(3, 4, 9); 
+    g.addEdge(3, 5, 14); 
+    g.addEdge(4, 5, 10); 
+    g.addEdge(5, 6, 2); 
+    g.addEdge(6, 7, 1); 
+    g.addEdge(6, 8, 6); 
+    g.addEdge(7, 8, 7); 
+  
+    /*vector<int> path = g.dijkstra(0, 4); 
+    showlist(path);
+    cout << "\n";
+    int cost = g.dist[4];
+    cout << cost << endl;
+    */
+    vector<vector<int>> A;
+    // shortest path from node 0 to 4, K = 8
+    A = yen(g, 0, 4, 8);
+    for(auto it = A.begin(); it != A.end(); ++it) {
+        showlist(*it); 
+        cout << "\n";
+    }
+    return 0; 
+} 
---
+++ yen.h
@@ -0,0 +1,48 @@
+#ifndef YEN_H
+#define YEN_H
+
+#include "stdc++.h"
+#include <iostream>
+
+using namespace std; 
+namespace YEN {
+
+// iPair ==>  Integer Pair 
+typedef pair<int, int> iPair; 
+typedef pair<int, vector<int>> vPair;
+
+class Graph 
+{ 
+public:
+    int V;
+    list< pair<int, int> > *adj; 
+    vector<int> dist;  // to store distance from source
+ 
+    Graph(int V); // Constructor  
+    Graph(const Graph &rhs);
+
+    // function to add an edge to graph 
+    void addEdge(int u, int v, int w);
+
+    void delEdge(int u, int v);
+
+    vector<int> findPath(int parent[], int s, int d);
+    // prints shortest path from s 
+    vector<int> dijkstra(int src, int des);
+};
+
+//void showlist(vector<int> g);
+void showlist(vector<int> g) { 
+    for(auto it = g.begin(); it != g.end(); ++it) 
+        cout << *it << ' '; 
+} 
+
+// helper function to slice vector from X to Y, inclusive
+//vector<int> slicing(vector<int>& arr, int X, int Y); 
+
+// psudocode: https://en.wikipedia.org/wiki/Yen%27s_algorithm
+vector<vector<int>> yen(Graph g, int s, int d, int K);
+
+}
+
+#endif
